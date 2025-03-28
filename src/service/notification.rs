52b54f990bd2887d7b2b52b3c143d92a6dcaa908
Muhammad Jordan <jordan.aziz18@gmail.com>
2025-03-28 13:41:35 +0700

    change notification service abit

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -16,6 +16,7 @@ impl NotificationService {
         let subscriber_result: Subscriber = SubscriberRepository::add(product_type_str, subscriber);
         return Ok(subscriber_result);
     }
+
     pub fn unsubscribe(product_type: &str, url: &str) -> Result<Subscriber> {
         let product_type_upper: String = product_type.to_uppercase();
         let product_type_str: &str = product_type_upper.as_str();
